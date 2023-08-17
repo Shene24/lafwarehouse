d3b6076de3059eacd7274227e075ed5c1cba3206
Nina2498 <shene1998m@gmail.com>
2023-08-17 10:13:08 +0300

    registration&login completed

--- login.php
+++ login.php
@@ -1,29 +1,32 @@
-<?php
-session_start(); // Start or resume the session
-
+<?php 
 require_once("config.php");
-/// Process form submission
-if ($_SERVER["REQUEST_METHOD"] == "POST") {
+session_start();
+
+if ($_SERVER["REQUEST_METHOD"] == "POST") { 
     $username = $_POST["username"];
     $password = $_POST["password"];
 
     // Validate user credentials
-    $sql = "SELECT * FROM users WHERE name='$username' AND password='$password'";
+    $sql = "SELECT * FROM users WHERE username='$username'";
     $result = $conn->query($sql);
 
     if ($result->num_rows == 1) {
         $user = $result->fetch_assoc();
+        
+        if (password_verify($password, $user["password"])) {
+            // Set session variables
+            $_SESSION["username"] = $user["username"];
+            $_SESSION["role"] = $user["role"];
+            $_SESSION["loggedin"] = true;
 
-        // Set session variables
-        $_SESSION["username"] = $user["name"];
-        $_SESSION["role"] = $user["role"];
-        $_SESSION["loggedin"] = true;
-
-        // Redirect to the index.html page
-        header("Location: index.html");
-        exit;
+            // Redirect to the index.html page
+            header("Location: index.html");
+            exit;
+        } else {
+            echo "Login failed. Invalid password.";
+        }
     } else {
-        echo "Login failed. Invalid username or password.";
+        echo "Login failed. User not found.";
     }
 }
 
@@ -38,7 +41,7 @@ $conn->close();
     <style>
         body {
             font-family: Arial, sans-serif;
-            background-color: #f2f2f2;
+            background-color:#191825;
             margin: 0;
             padding: 0;
             display: flex;
@@ -59,19 +62,19 @@ $conn->close();
         }
         .login-form input[type="text"],
         .login-form input[type="password"] {
-            width: 100%;
+            width: 80%;
             padding: 10px;
             margin-bottom: 15px;
             border: 1px solid #ccc;
             border-radius: 5px;
         }
         .login-form input[type="submit"] {
-            background-color: #007bff;
+            background-color: #3c3a5a;
             color: #fff;
             border: none;
             padding: 10px;
             border-radius: 5px;
-            width: 100%;
+            width: 50%;
             cursor: pointer;
             transition: background-color 0.3s ease;
         }
@@ -82,14 +85,16 @@ $conn->close();
 </head>
 <body>
     <div class="login-container">
-        <h2>Login</h2>
+        <h2>User Login</h2>
         <form class="login-form" action="login.php" method="post">
             <label for="username">Username:</label>
             <input type="text" name="username" required>
-            <label for="password">Password:</label>
+            <label for="password">Phone:</label>
             <input type="password" name="password" required>
             <input type="submit" value="Login">
         </form>
+        <p>Don't have an account? <a href="registration.php">Register here</a></p>
     </div>
 </body>
 </html>
+
